5db9756b4150177ee3ad1f0d49779c6152f91403
Amir Savand <amir@savandbros.com>
2022-03-07 12:26:57 +0300

    Release v1.1.1
    Update server address

--- src/environments/environment.dev-production.ts
+++ src/environments/environment.dev-production.ts
@@ -3,7 +3,7 @@ import { Environment } from '@app/shared/interfaces/environment';
 export const environment: Environment = {
   development: true,
   name: 'production',
-  api: 'https://api.chatminal.savandbros.com/',
+  api: 'https://chatminal-api.savandbros.com/',
   pusherKey: '52a1b54e390a2fff28b2',
   pusherCluster: 'eu',
 };
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -3,7 +3,7 @@ import { Environment } from '@app/shared/interfaces/environment';
 export const environment: Environment = {
   development: false,
   name: 'staging',
-  api: 'https://api-staging.chatminal.savandbros.com/',
+  api: 'https://chatminal-staging-api.savandbros.com/',
   pusherKey: '52a1b54e390a2fff28b2',
   pusherCluster: 'eu',
 };
